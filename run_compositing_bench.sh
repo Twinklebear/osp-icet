d9604e64fcc284c69e186f3bec910594150711d9
Will Usher <willusher.life@gmail.com>
2018-01-26 16:04:59 -0600

    improving scripts to properly get thread affinity???

--- run_compositing_bench.sh
+++ run_compositing_bench.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+echo "OSPRAY_THREADS= $OSPRAY_THREADS"
 set -x
-echo $OSPRAY_THREADS
 ibrun ./benchmark -compositor $BENCH_COMPOSITOR -n $BENCH_ITERS \
   -img $IMAGE_SIZE_X $IMAGE_SIZE_Y
 
--- submit_compositing_scaling.sh
+++ submit_compositing_scaling.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-export IMAGE_SIZE_X=2048
-export IMAGE_SIZE_Y=2048
-export BENCH_ITERS=250
+export IMAGE_SIZE_X=1024
+export IMAGE_SIZE_Y=1024
+export BENCH_ITERS=125
 
 if [ "`hostname -d`" == "stampede2.tacc.utexas.edu" ]; then
-  echo "Setting OSPRAY_THREADS=68 for KNL"
   export OSPRAY_THREADS=68
+  export MACHINE=stampede2
+elif [ "`hostname -d`" == "ls5.tacc.utexas.edu" ]; then
+  export OSPRAY_THREADS=20
+  export MACHINE=ls5
 fi
 
 script_dir=$(dirname $(readlink -f $0))
 
-compositors=(ospray icet)
+compositors=(ospray) # icet)
 node_counts=(2 4 8 16 32 64)
 for c in "${compositors[@]}"; do
   export BENCH_COMPOSITOR=$c
   for i in "${node_counts[@]}"; do
     job_title="bench_${c}_${i}n_${IMAGE_SIZE_X}x${IMAGE_SIZE_Y}"
-    sbatch -n $i -N $i --ntasks-per-node=1 -t 00:15:00 \
-      -A OSPRay -p normal \
+    sbatch -n $i -N $i --ntasks-per-node=1 -t 00:30:00 \
+      -A OSPRay -p normal -S $OSPRAY_THREADS \
       -J $job_title -o ${job_title}.txt \
       ${script_dir}/run_compositing_bench.sh
   done
