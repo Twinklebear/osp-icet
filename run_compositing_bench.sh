f43ca6724a7ece665e689e26fa6dbfb540aec684
Will Usher <will@sci.utah.edu>
2018-01-27 21:31:13 +0000

    Support for Theta as well
    The cobalt job submission system really sucks.

--- run_compositing_bench.sh
+++ run_compositing_bench.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "OSPRAY_THREADS= $OSPRAY_THREADS"
+echo "OSPRAY_THREADS=$OSPRAY_THREADS"
+BENCH_ARGS="-compositor $BENCH_COMPOSITOR -n $BENCH_ITERS -img $IMAGE_SIZE_X $IMAGE_SIZE_Y"
 if [ -n "$TACC" ]; then
-	ibrun ./benchmark -compositor $BENCH_COMPOSITOR -n $BENCH_ITERS \
-		-img $IMAGE_SIZE_X $IMAGE_SIZE_Y
+	ibrun ./benchmark $BENCH_ARGS
 elif [ "$MACHINE" == "wopr" ]; then
-	mpirun -np $SLURM_JOB_NUM_NODES -ppn 1 \
-		./benchmark -compositor $BENCH_COMPOSITOR -n $BENCH_ITERS \
-		-img $IMAGE_SIZE_X $IMAGE_SIZE_Y
+	mpirun -np $SLURM_JOB_NUM_NODES -ppn 1 ./benchmark $BENCH_ARGS
+elif [ "$MACHINE" == "theta" ]; then
+	aprun -n $THETA_USE_NODES -N 1 -d 64 -cc depth ./benchmark $BENCH_ARGS
 fi
 
--- submit_compositing_scaling.sh
+++ submit_compositing_scaling.sh
@@ -9,39 +9,60 @@ if [ "$CLUSTER_NAME" == "stampede2.tacc.utexas.edu" ]; then
 	export OSPRAY_THREADS=68
 	export MACHINE=stampede2
 	export TACC=true
-  export JOB_QUEUE=normal
+	export JOB_QUEUE=normal
 elif [ "$CLUSTER_NAME" == "ls5.tacc.utexas.edu" ]; then
 	export OSPRAY_THREADS=20
 	export MACHINE=ls5
 	export TACC=true
-  export JOB_QUEUE=normal
+	export JOB_QUEUE=normal
 elif [ "$CLUSTER_NAME" == "maverick.tacc.utexas.edu" ]; then
 	export OSPRAY_THREADS=20
 	export MACHINE=maverick
 	export TACC=true
-  export JOB_QUEUE=vis
+	export JOB_QUEUE=vis
 elif [ "`hostname`" == "wopr.sci.utah.edu" ]; then
 	export OSPRAY_THREADS=32
 	export MACHINE=wopr
-  export JOB_QUEUE=normal
+	export JOB_QUEUE=normal
+elif [ "`hostname | head -c 5`" == "theta" ]; then
+	export OSPRAY_THREADS=64
+	export MACHINE=theta
 fi
 
 script_dir=$(dirname $(readlink -f $0))
 
 compositors=(ospray icet)
-node_counts=(2 4 8 16 32)
+node_counts=(2 4 8 16 32 64)
 for c in "${compositors[@]}"; do
 	export BENCH_COMPOSITOR=$c
 	for i in "${node_counts[@]}"; do
 		job_title="bench_${c}_${i}n_${IMAGE_SIZE_X}x${IMAGE_SIZE_Y}"
+
 		if [ -n "$TACC" ]; then
 			export TACC_ARGS="-A OSPRay -p $JOB_QUEUE"
 		fi
-		sbatch -n $i -N $i --ntasks-per-node=1 -t 01:00:00 \
-			$TACC_ARGS \
-			-S $OSPRAY_THREADS \
-			-J $job_title -o ${job_title}.txt \
-			${script_dir}/run_compositing_bench.sh
+		if [ -n "`command -v sbatch`" ]; then
+			sbatch -n $i -N $i --ntasks-per-node=1 -t 00:20:00 \
+				$TACC_ARGS \
+				-S $OSPRAY_THREADS \
+				-J $job_title -o ${job_title}.txt \
+				${script_dir}/run_compositing_bench.sh
+		elif [ -n "`command -v qsub`" ]; then
+			# A lot of crap for theta and qsub because cobalt is
+			# dumb to pick up my environment
+			THETA_JOB_NODES=$(( $i > 8 ? $i : 8 ))
+			export THETA_USE_NODES=$i
+			qsub -n $THETA_JOB_NODES -t 00:30:00 -A Viz_Support \
+				-o ${job_title}.txt \
+				--env "MACHINE=$MACHINE" \
+				--env "OSPRAY_THREADS=$OSPRAY_THREADS" \
+				--env "IMAGE_SIZE_X=$IMAGE_SIZE_X" \
+				--env "IMAGE_SIZE_Y=$IMAGE_SIZE_Y" \
+				--env "BENCH_ITERS=$BENCH_ITERS" \
+				--env "BENCH_COMPOSITOR=$BENCH_COMPOSITOR" \
+				--env "THETA_USE_NODES=$THETA_USE_NODES" \
+				${script_dir}/run_compositing_bench.sh				
+		fi
 	done
 done
 
