9ecd10ef162cb69a9fe5afd95e44d86c57a37d0b
Will Usher <will@sci.utah.edu>
2018-04-04 12:25:11 -0500

    script tweaks

--- submit_compositing_scaling.sh
+++ submit_compositing_scaling.sh
@@ -3,33 +3,34 @@
 export IMAGE_SIZE_X=1024
 export IMAGE_SIZE_Y=1024
 export BENCH_ITERS=200
+export I_MPI_PIN_DOMAIN=node
 
 export CLUSTER_NAME="`hostname -d`"
 if [ "$CLUSTER_NAME" == "stampede2.tacc.utexas.edu" ]; then
 	export OSPRAY_THREADS=68
 	export MACHINE=stampede2
 	export TACC=true
-  export JOB_QUEUE=normal
+	export JOB_QUEUE=normal
 elif [ "$CLUSTER_NAME" == "ls5.tacc.utexas.edu" ]; then
 	export OSPRAY_THREADS=20
 	export MACHINE=ls5
 	export TACC=true
-  export JOB_QUEUE=normal
+	export JOB_QUEUE=normal
 elif [ "$CLUSTER_NAME" == "maverick.tacc.utexas.edu" ]; then
 	export OSPRAY_THREADS=20
 	export MACHINE=maverick
 	export TACC=true
-  export JOB_QUEUE=vis
+	export JOB_QUEUE=vis
 elif [ "`hostname`" == "wopr.sci.utah.edu" ]; then
 	export OSPRAY_THREADS=32
 	export MACHINE=wopr
-  export JOB_QUEUE=normal
+	export JOB_QUEUE=normal
 fi
 
 script_dir=$(dirname $(readlink -f $0))
 
 compositors=(ospray icet)
-node_counts=(2 4 8 16 32)
+node_counts=(2 4 8 16 32 64)
 for c in "${compositors[@]}"; do
 	export BENCH_COMPOSITOR=$c
 	for i in "${node_counts[@]}"; do
@@ -37,7 +38,7 @@ for c in "${compositors[@]}"; do
 		if [ -n "$TACC" ]; then
 			export TACC_ARGS="-A OSPRay -p $JOB_QUEUE"
 		fi
-		sbatch -n $i -N $i --ntasks-per-node=1 -t 01:00:00 \
+		sbatch -n $i -N $i --ntasks-per-node=1 -t 00:15:00 \
 			$TACC_ARGS \
 			-S $OSPRAY_THREADS \
 			-J $job_title -o ${job_title}.txt \
