7587af65065a3868fb64d81a71216b2e9f55725c
Will Usher <will@sci.utah.edu>
2019-12-26 17:25:09 -0600

    fixed setenvvar for skx

--- set_ospray_vars.sh
+++ set_ospray_vars.sh
@@ -12,7 +12,6 @@ set_ospray_env_vars() {
 	export I_MPI_PIN_RESPECT_CPUSET=0
 	export I_MPI_PIN_RESPECT_HCA=0
 	export I_MPI_PIN_DOMAIN=omp
-	export I_MPI_PIN_PROCESSOR_LIST=allcores
 	export OSPRAY_SET_AFFINITY=0
 
 	CPU_MODEL=`cat /proc/cpuinfo | grep "model name" | uniq`
@@ -21,9 +20,11 @@ set_ospray_env_vars() {
 		if [ -n "`echo $CPU_MODEL | grep 'Xeon(R)'`" ]; then
 			echo "CPU $CPU_MODEL is Xeon, using all logical cores"
 			export OSPRAY_THREADS=$(get_logical_cores)
+            export I_MPI_PIN_PROCESSOR_LIST=all
 		else
 			echo "CPU $CPU_MODEL is Xeon Phi, using all physical cores"
 			export OSPRAY_THREADS=$(get_physical_cores)
+            export I_MPI_PIN_PROCESSOR_LIST=allcores
 		fi
 	else
 		echo "OSPRAY_THREADS set by user to $OSPRAY_THREADS"
@@ -37,5 +38,5 @@ set_ospray_env_vars() {
 	export MPICH_MAX_THREAD_SAFETY=multiple
 }
 
-set_ospray_env_vars
+printenv
 
