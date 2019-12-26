f9796f077a6dfef6f37681583dfd15d329129c51
Will Usher <will@sci.utah.edu>
2019-12-25 20:33:41 -0600

    bench scripts

--- run_compositing_bench.sh
+++ run_compositing_bench.sh
@@ -1,71 +1,25 @@
 #!/bin/bash
 
-if [ -n "$TACC" ]; then
-	module restore
-fi
-
-source ${SCRIPT_DIR}/set_ospray_vars.sh
+module restore
 
 if [ -n "$WORK_DIR" ]; then
 	echo "Changing to $WORK_DIR"
 	cd $WORK_DIR
 fi
 
+REPO_ROOT=`git rev-parse --show-toplevel`
+echo "Script dir = $REPO_ROOT"
+source $REPO_ROOT/set_ospray_vars.sh
+
 # Only one of these will be non-empty
 JOBID="${SLURM_JOBID}${COBALT_JOBID}"
 NPROCS="${SLURM_NNODES}${COBALT_PARTSIZE}"
 
-compositors=(ospray icet)
+compositors=(dfb icet)
 for c in "${compositors[@]}"; do
-	export JOB_NAME="bench_${c}_${NPROCS}n_${IMAGE_SIZE_Y}x${IMAGE_SIZE_Y}-${JOBID}"
-	if [ -n "$JOB_QUEUE" ]; then
-		export JOB_NAME="${JOB_NAME}-$JOB_QUEUE"
-	fi
-	if [ -n "$PREFIX" ]; then
-		export JOB_NAME="${PREFIX}-${JOB_NAME}"
-	fi
+	export LOGFILE="bench_${c}_${NPROCS}n.txt"
+	export BENCH_ARGS="-$c ${JSON_CONFIG}"
 
-	export OSPRAY_JOB_NAME="$SCRATCH/osp-icet/${JOB_NAME}"
-	export BENCH_ARGS="-compositor $c \
-		-n $BENCH_ITERS \
-		-img $IMAGE_SIZE_X $IMAGE_SIZE_Y \
-		-o $OSPRAY_JOB_NAME"
-	logfile=${OSPRAY_JOB_NAME}.txt
-
-	printenv > $logfile
-
-	if [ -n "$TACC" ]; then
-		ibrun $TRACE_ARG $BUILD_DIR/benchmark $BENCH_ARGS >> $logfile 2>&1
-	elif [ "$MACHINE" == "wopr" ]; then
-		mpirun -np $SLURM_JOB_NUM_NODES -ppn 1 $BUILD_DIR/benchmark $BENCH_ARGS >> $logfile 2>&1
-	elif [ "$MACHINE" == "theta" ]; then
-		# On Theta we run jobs for 2-128 nodes on the same 128 node allocation
-		# since 128 is the min size.
-		if [ "$COBALT_PARTSIZE" == "128" ]; then
-			node_counts=(4 8 16 32 64 128)
-			for i in "${node_counts[@]}"; do
-				export JOB_NAME="bench_${c}_${i}n_${IMAGE_SIZE_Y}x${IMAGE_SIZE_Y}-${JOBID}"
-				if [ -n "$JOB_QUEUE" ]; then
-					export JOB_NAME="${JOB_NAME}-$JOB_QUEUE"
-				fi
-				if [ -n "$PREFIX" ]; then
-					export JOB_NAME="${PREFIX}-${JOB_NAME}"
-				fi
-				export OSPRAY_JOB_NAME="$SCRATCH/osp-icet/${JOB_NAME}"
-				logfile=${OSPRAY_JOB_NAME}.txt
-
-				export BENCH_ARGS="-compositor $c \
-					-n $BENCH_ITERS \
-					-img $IMAGE_SIZE_X $IMAGE_SIZE_Y \
-					-o $OSPRAY_JOB_NAME"
-
-				echo "Running $JOB_NAME"
-				printenv > $logfile
-				aprun -n $i -N 1 -d 64 -cc depth $BUILD_DIR/benchmark $BENCH_ARGS >> $logfile 2>&1
-			done
-		else
-			aprun -n $COBALT_PARTSIZE -N 1 -d 64 -cc depth $BUILD_DIR/benchmark $BENCH_ARGS >> $logfile 2>&1
-		fi
-	fi
+	ibrun ./osp_icet $BENCH_ARGS > $LOGFILE 2>&1
 done
 
--- set_ospray_vars.sh
+++ set_ospray_vars.sh
@@ -5,17 +5,21 @@ get_physical_cores() {
 }
 
 set_ospray_env_vars() {
-	export I_MPI_PIN_RESPECT_CPUSET=0
-	export I_MPI_PIN_RESPECT_HCA=0
-	export I_MPI_PIN_DOMAIN=omp
-	export I_MPI_PIN_PROCESSOR_LIST=allcores
-	export OSPRAY_SET_AFFINITY=0
-	# If it's not set (i.e, we're not on Theta) figure out the node's phys. cores
-	# On theta this script runs on the mom node, and the core count would be wrong
-	if [ -z "$OSPRAY_THREADS"]; then
-		export OSPRAY_THREADS=$(get_physical_cores)
-	fi
-	export OMP_NUM_THREADS=$OSPRAY_THREADS
+#	export I_MPI_PIN_RESPECT_CPUSET=0
+#	export I_MPI_PIN_RESPECT_HCA=0
+#	export I_MPI_PIN_DOMAIN=omp
+#	export I_MPI_PIN_PROCESSOR_LIST=allcores
+#	export OSPRAY_SET_AFFINITY=0
+#	# If it's not set (i.e, we're not on Theta) figure out the node's phys. cores
+#	# On theta this script runs on the mom node, and the core count would be wrong
+#	if [ -z "$OSPRAY_THREADS"]; then
+#		export OSPRAY_THREADS=$(get_physical_cores)
+#	fi
+#	export OMP_NUM_THREADS=$OSPRAY_THREADS
+	source $EMBREE_DIR/embree-vars.sh
+	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${OSPRAY_DIR}/build/install/lib64/
+	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${OPENVKL_DIR}/build/install/lib64/
+	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${OSPCOMMON_DIR}/build/install/lib/
 	export MPICH_MAX_THREAD_SAFETY=multiple
 }
 
